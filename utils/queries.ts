b667de50d11e3e51dc2520051e1ed47952320375
Ikram <ikramzulfugar@gmail.com>
2022-10-23 03:50:39 +0400

    Added new queries, and expanded existings

--- utils/queries.ts
+++ utils/queries.ts
@@ -31,7 +31,8 @@ export const allPostsQuery = () => {
         image
       },
     },
-    _createdAt
+    _createdAt,
+    _updatedAt
   }`;
 
   return query;
@@ -71,7 +72,8 @@ export const filterPostsByQuery = (filter: string | string[]) => {
           image
         }
       },
-      _createdAt
+      _createdAt,
+      _updatedAt
     }
   `
 
@@ -113,14 +115,61 @@ export const postDetailQuery = (postId: string | string[]) => {
           image
         }
       },
-      _createdAt
+      _createdAt,
+      _updatedAt
     }
     `
   return query;
 };
 
 export const singleUserQuery = (userId: string | string[]) => {
-  const query = `*[_type == "user" && _id == '${userId}']`;
+  const query = 
+  `
+  *[_type == "user" && _id == "${userId}"]{
+    ... ,
+    "posts": *[_type == "post" && userId == ^._id]{
+      _id,
+      caption,
+      topic,
+      video {
+        asset-> {
+          _id,
+          url
+        }
+      },
+      image{
+        asset->{
+          _id,
+          url
+        }
+      },
+      userId,
+      postedBy->{
+        _id,
+        username,
+        image
+      },
+      likes,
+      _createdAt,
+      comments[]{
+        comment,
+        _key,
+        postedBy->{
+          _id,
+          username,
+          image
+        }
+      },
+      _createdAt,
+      _updatedAt
+    },
+  } {
+    ...,
+    "totalPosts": count(posts),
+    "totalLikes": length(posts[].likes[]),
+    "totalComments": length(posts[].comments[])
+  }[0]
+  `;
 
   return query;
 };
@@ -142,6 +191,12 @@ export const userCreatedPostsQuery = (userId: string | string[]) => {
         url
       }
     },
+    image{
+      asset->{
+        _id,
+        url
+      }
+    },
     userId,
     postedBy->{
       _id,
@@ -158,7 +213,8 @@ export const userCreatedPostsQuery = (userId: string | string[]) => {
         image
       },
     },
-    _createdAt
+    _createdAt,
+    _updatedAt
   }`;
 
   return query;
@@ -196,8 +252,29 @@ export const userLikedPostsQuery = (userId: string | string[]) => {
         image
       },
     },
-    _createdAt
+    _createdAt,
+    _updatedAt
   }`;
 
   return query;
 };
+
+export const usersOfMostCommentedAndLikedPost = () => {
+  const query = 
+  `
+  *[_type == "user"] {
+    image,
+    _id,
+    "posts": *[_type == "post" && userId == ^._id] {
+      "commentsLength": length(comments),
+      "likesLength": length(likes)
+    },
+    username,
+  }[
+    math::sum([...posts[].commentsLength,...posts[].likesLength]) > 0
+  ] | order(math::sum([...posts[].commentsLength,...posts[].likesLength]) desc)[0...5]
+
+  `
+  return query
+}
+
